2a6a0722b5c6d34370432786148f4f3bc63a82d6
Matt Roberts <justskiit@gmail.com>
2020-06-06 09:48:31 -0600

    Get All, one, put, post and delete completed testing

--- db/Message.js
+++ db/Message.js
@@ -1,9 +1,18 @@
 //Message Model
 // If you are using mongo/mongoose create your schema here
-
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 const db = require('./mongoconfig');
 
-const Message = null;
+const messageSchema = new Schema({
+    id: Number,
+    name: String,
+    message: String
+});
+
+let Message = mongoose.model('Message', messageSchema);
 
 
 module.exports = Message;
+
+
--- server.js
+++ server.js
@@ -1,15 +1,65 @@
 const express = require('express');
 const app = express();
+const Message = require('./db/Message');
+const bodyParser = require('body-parser');
 
-// if you are using mongo/mongoose uncomment this line
-// const Message = require('./db/Message');
+app.use(bodyParser.json());
 
-// if you are using postgres, uncomment this line
-// const pool = require('./db/pgconfig');
+//Get all messages
+app.get('/api/messages', function(req, res) {
+  let allMessages = Message.find(function(err, data) {
+    res.send(data)
+    console.log(data)
+  });
+})
 
-app.use((req,res,next) => {
-  res.status(404).send('That route does not exist');
+//Get one message
+app.get('/api/messages/user/:id', function(req, res) {
+  let idObj = req.params
+  console.log('req.params:', req.params)
+  Message.find( idObj ).exec(function (err, message){
+    if (err) throw ('err')
+    res.status(200).send(message)
+    console.log("message", message)
+  })
 });
+
+// If your route is /user/:id and you make a request to /user/5 - req.params would yield {id: "5"}
+
+//api/message?id=7
+
+//Post one message
+app.post('/api/messages', function (req, res, next) {
+  let oneMessage = new Message({ id: 876, name: 'Yoda', message: 'The dark side, he turns to!'})
+  oneMessage.save(function (err) {
+    if (err) throw ('cleanup on aisle post')
+    console.log(oneMessage)
+  })
+  res.status(200).send('Posted your message, have a nice day');
+});
+
+//Update message
+app.put('/api/messages', function(req, res, next){
+  console.log("updated!!!");
+  let query = { name: 'James Bond'}
+  Message.updateOne(query, { name: 'Ethan Parent'}, function(err, data) {
+    res.status(200).send('UPDATED!');
+  })
+});
+
+
+//Delete message
+app.delete('/api/messages', function (req, res, next) {
+  console.log('Boom, deleted!');
+  let query = { name: 'Neo'};
+  Message.deleteOne(query, function(err){
+    if (err) throw ('Clean up on aisle DELETE!');
+  })
+  res.status(200).send('Clean up on aisle DELETE!');
+})
+
+
+
 
 const port = 3000;
 
